c7d041d13e248816bab68280899900509fc998a8
riz1-lv <riz.shkh1@gmail.com>
2021-04-12 16:31:17 -0400

    made color changes easier

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,6 +12,7 @@
 </template>
 
 <script>
+const color = "#2ec4b6";
 import VueParticle from "vue-particlejs";
 export default {
   name: "HelloWorld",
@@ -26,10 +27,10 @@ export default {
       particleConfig: {
         particles: {
           number: { value: 150, density: { enable: true, value_area: 800 } },
-          color: { value: "#000000" },
+          color: { value: color },
           shape: {
             type: "circle",
-            stroke: { width: 0, color: "#000000" },
+            stroke: { width: 0, color: color },
             polygon: { nb_sides: 5 },
             image: { src: "img/github.svg", width: 100, height: 100 }
           },
@@ -46,7 +47,7 @@ export default {
           line_linked: {
             enable: true,
             distance: 150,
-            color: "#555555",
+            color: color,
             opacity: 0.4,
             width: 3
           },
